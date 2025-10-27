2679604dd41809f8b00b736b01e1b91bc8053be6
Edna Wangui <your-email@example.com>
2025-10-27 22:53:30 +0300

    Completed Phase 4: Added authentication and finalized Budget & Expense API endpoints

--- budget_backend/settings.py
+++ budget_backend/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'tracker', 
+    'rest_framework.authtoken',
+    'tracker',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
+
+
--- tracker/views.py
+++ tracker/views.py
@@ -1,32 +1,29 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import Budget, Expense
 from .serializers import BudgetSerializer, ExpenseSerializer
 
-# 💰 Budget Views
+
 class BudgetListCreateView(generics.ListCreateAPIView):
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Require login
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)  # Automatically assign logged-in user
+    permission_classes = [AllowAny]
 
 
 class BudgetDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class ExpenseListCreateView(generics.ListCreateAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    # permission_classes = [permissions.IsAuthenticated]  # enable later
-    def perform_create(self, serializer):
-        serializer.save()
+    permission_classes = [AllowAny]
 
 
 class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [AllowAny]
+
